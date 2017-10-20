830dd9399af992c98a1bd48fdfd65b718ee22a9d
Sumaiya4949 <32925935+Sumaiya4949@users.noreply.github.com>
2017-10-20 21:44:49 +0600

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,21 @@
+
+package basicjava;
+
+import java.util.Scanner;
+
+public class Star {
+    public static void main(String[] args) {
+         int i;
+        Scanner input = new Scanner(System.in);
+        
+        
+        for(i=1;i<=6;i++){
+            for(int j=1;j<i;j++){
+                       System.out.print("*");   
+    }
+            System.out.println(""); 
+    }
+     
+    }
+   
+}
