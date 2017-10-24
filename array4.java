fa04abb40cb63e00f26890d493cc46cf7c47fe10
Sumaiya4949 <32925935+Sumaiya4949@users.noreply.github.com>
2017-10-24 22:58:23 +0600

    Add files via upload

--- array4.java
+++ array4.java
@@ -5,10 +5,35 @@
  */
 package basicjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DIU
  */
 public class array4 {
+    public static void main(String[] args) {
+        int i;
+        int count=0;
+        
+        int[] number = new int[5];
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter 5 number");
+        for( i=0;i<number.length;i++){
+        number[i] = input.nextInt();
+        }
+         System.out.println("\n\n");
+        for( i=0;i<number.length;i++){
+          if(number[i]<10){
+             count++;
+          }
+           
+          
+    }
+         System.out.println("The number of digit less than 10 is "+count);
+    
+}
+
     
 }
