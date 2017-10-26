9667920e5d57355b42067c1f9a66aa6f8309ca5e
Sumaiya4949 <32925935+Sumaiya4949@users.noreply.github.com>
2017-10-26 14:51:27 +0600

    Update array8.java

--- array8.java
+++ array8.java
@@ -5,10 +5,41 @@
  */
 package basicjava;
 
+import java.util.Scanner;
+
 /**
  *
- * @author DIU
+ * @author Administrator
  */
 public class array8 {
+ public static void main(String[] args) {
+        
+    
+
+    int i,j=0;
+        
+       
+        int[] number = new int[5];
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter 5 number");
+        for( i=0;i<number.length;i++){
+        number[i] = input.nextInt();
+        }
+         System.out.println("\n");
+          int temp = number[0];
+        for( i=0;i<number.length;i++){
+          {
+             if(temp<number[i]){
+                 temp = number[i];
+                 j=i;
+             }
+             
+          }
+            
     
 }
+        System.out.println("Largest number "+temp+ " Index is "+(j+1));
+    }
+}
+ 
